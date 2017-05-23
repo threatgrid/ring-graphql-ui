cd4da1dd96fdbd2f3772e4b91457fef6c2059bda
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2017-05-23 16:47:44 +0200

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ring-graphql-ui "0.1.1-SNAPSHOT"
+(defproject threatgrid/ring-graphql-ui "0.1.1"
   :description "GraphQL UI for Ring apps."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
